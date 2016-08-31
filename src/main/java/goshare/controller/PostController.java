17e71ee6d971f51b559db100b30dab0c40ff3010
xumeng@gmail.com <xumeng@gmail.com>
2016-08-31 16:37:36 -0400

    cor

--- src/main/java/goshare/controller/PostController.java
+++ src/main/java/goshare/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import goshare.repository.PostRepository;
  */
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class PostController {
 	
